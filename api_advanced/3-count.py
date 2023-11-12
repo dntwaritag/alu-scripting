3a4aa0889fbb3c1901010d7ecfebe0b4b9bc4bc5
dntwaritag <d.ntwaritag@alustudent.com>
2023-11-11 21:48:06 -0800

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,44 +1,29 @@
 #!/usr/bin/python3
-"""Module documentation"""
+"""
+Uses https://jsonplaceholder.typicode.com to return information about all
+employee's todo list progress
+"""
+
 import json
 import requests
 
-
-def count_words(subreddit, word_list, after='', hot_list=None):
-    if hot_list is None:
-        hot_list = [0] * len(word_list)
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url, params={'after': after},
-                           allow_redirects=False,
-                           headers={'User-Agent': 'My User Agent 1.0'})
-
-    if request.status_code == 200:
-        data = request.json()
-
-        for topic in data['data']['children']:
-            title_words = topic['data']['title'].split()
-            for word in title_words:
-                for i, keyword in enumerate(word_list):
-                    if keyword.lower() == word.lower():
-                        hot_list[i] += 1
-
-        after = data['data']['after']
-        if after is None:
-            word_counts = {}
-            for i, word in enumerate(word_list):
-                if word not in word_counts:
-                    word_counts[word] = hot_list[i]
-                else:
-                    word_counts[word] += hot_list[i]
-
-            sorted_counts = sorted(
-                word_counts.items(),
-                key=lambda x: (-x[1], x[0].lower())
-            )
-
-            for word, count in sorted_counts:
-                if count > 0:
-                    print("{}: {}".format(word.lower(), count))
-        else:
-            count_words(subreddit, word_list, after, hot_list)
+if __name__ == '__main__':
+    users = requests.get("https://jsonplaceholder.typicode.com/users",
+                         verify=False).json()
+    userdict = {}
+    usernamedict = {}
+    for user in users:
+        uid = user.get("id")
+        userdict[uid] = []
+        usernamedict[uid] = user.get("username")
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos",
+                        verify=False).json()
+    for task in todo:
+        taskdict = {}
+        uid = task.get("userId")
+        taskdict["task"] = task.get('title')
+        taskdict["completed"] = task.get('completed')
+        taskdict["username"] = usernamedict.get(uid)
+        userdict.get(uid).append(taskdict)
+    with open("todo_all_employees.json", 'w') as jsonfile:
+        json.dump(userdict, jsonfile)
